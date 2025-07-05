6d7f3e6542399686acd34372552c5ea970f01d72
Rodrigo Inocenti <rinocenti8@gmail.com>
2025-07-05 14:07:33 -0300

    fix: build

--- adapters-in/src/main/java/org/example/NotificationMain.java
+++ adapters-in/src/main/java/org/example/NotificationMain.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
-public class Main {
+public class NotificationMain {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(NotificationMain.class, args);
     }
 }
